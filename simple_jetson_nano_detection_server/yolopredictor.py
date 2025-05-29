5687cc636fb604e85e4ab53f2266c8fb49324206
XuZhen86 <curt_xu@me.com>
2025-05-28 22:53:48 -0400

    Add event metrics tracking to YoloPredictor.

--- simple_jetson_nano_detection_server/yolopredictor.py
+++ simple_jetson_nano_detection_server/yolopredictor.py
@@ -1,9 +1,12 @@
+from enum import Enum, auto
 from tempfile import NamedTemporaryFile
 from typing import List, Optional, Tuple
 
 import ultralytics
 from absl import flags
+from line_protocol_cache.lineprotocolcache import LineProtocolCache
 
+from simple_jetson_nano_detection_server.eventmetricstracker import EventMetricsTracker
 from simple_jetson_nano_detection_server.prediction import Prediction
 
 _IMAGE_SIZE = flags.DEFINE_integer(
@@ -22,6 +25,93 @@ _HALF_PRECISION = flags.DEFINE_bool(
 )
 
 
+class _EventMetricsFields(Enum):
+  IMAGE_BYTES = auto()
+
+
+class _CocoCategories(Enum):
+  PERSON = 'person'
+  BICYCLE = 'bicycle'
+  CAR = 'car'
+  MOTORCYCLE = 'motorcycle'
+  AIRPLANE = 'airplane'
+  BUS = 'bus'
+  TRAIN = 'train'
+  TRUCK = 'truck'
+  BOAT = 'boat'
+  TRAFFIC_LIGHT = 'traffic light'
+  FIRE_HYDRANT = 'fire hydrant'
+  STOP_SIGN = 'stop sign'
+  PARKING_METER = 'parking meter'
+  BENCH = 'bench'
+  BIRD = 'bird'
+  CAT = 'cat'
+  DOG = 'dog'
+  HORSE = 'horse'
+  SHEEP = 'sheep'
+  COW = 'cow'
+  ELEPHANT = 'elephant'
+  BEAR = 'bear'
+  ZEBRA = 'zebra'
+  GIRAFFE = 'giraffe'
+  BACKPACK = 'backpack'
+  UMBRELLA = 'umbrella'
+  HANDBAG = 'handbag'
+  TIE = 'tie'
+  SUITCASE = 'suitcase'
+  FRISBEE = 'frisbee'
+  SKIS = 'skis'
+  SNOWBOARD = 'snowboard'
+  SPORTS_BALL = 'sports ball'
+  KITE = 'kite'
+  BASEBALL_BAT = 'baseball bat'
+  BASEBALL_GLOVE = 'baseball glove'
+  SKATEBOARD = 'skateboard'
+  SURFBOARD = 'surfboard'
+  TENNIS_RACKET = 'tennis racket'
+  BOTTLE = 'bottle'
+  WINE_GLASS = 'wine glass'
+  CUP = 'cup'
+  FORK = 'fork'
+  KNIFE = 'knife'
+  SPOON = 'spoon'
+  BOWL = 'bowl'
+  BANANA = 'banana'
+  APPLE = 'apple'
+  SANDWICH = 'sandwich'
+  ORANGE = 'orange'
+  BROCCOLI = 'broccoli'
+  CARROT = 'carrot'
+  HOT_DOG = 'hot dog'
+  PIZZA = 'pizza'
+  DONUT = 'donut'
+  CAKE = 'cake'
+  CHAIR = 'chair'
+  COUCH = 'couch'
+  POTTED_PLANT = 'potted plant'
+  BED = 'bed'
+  DINING_TABLE = 'dining table'
+  TOILET = 'toilet'
+  TV = 'tv'
+  LAPTOP = 'laptop'
+  MOUSE = 'mouse'
+  REMOTE = 'remote'
+  KEYBOARD = 'keyboard'
+  CELL_PHONE = 'cell phone'
+  MICROWAVE = 'microwave'
+  OVEN = 'oven'
+  TOASTER = 'toaster'
+  SINK = 'sink'
+  REFRIGERATOR = 'refrigerator'
+  BOOK = 'book'
+  CLOCK = 'clock'
+  VASE = 'vase'
+  SCISSORS = 'scissors'
+  TEDDY_BEAR = 'teddy bear'
+  HAIR_DRIER = 'hair drier'
+  TOOTHBRUSH = 'toothbrush'
+
+
 class YoloPredictor:
 
   _model: Optional[ultralytics.YOLO] = None
@@ -34,6 +124,7 @@ class YoloPredictor:
   def predict(cls, image_data: bytes) -> List[Prediction]:
     assert cls._model is not None, 'A model must be set before prediciton'
 
+    cls._record_image_size(image_data)
     with NamedTemporaryFile(dir='/dev/shm', suffix='.jpg') as image_file:
       image_file.write(image_data)
       image_file.flush()
@@ -65,4 +156,22 @@ class YoloPredictor:
               label=str(cls._model.names.get(int(class_id))),
           ))
 
+    cls._record_coco_categories(predictions)
     return predictions
+
+  @classmethod
+  def _record_image_size(cls, image_data: bytes) -> None:
+    tracker: EventMetricsTracker[_EventMetricsFields] = EventMetricsTracker()
+    tracker.record(_EventMetricsFields.IMAGE_BYTES, len(image_data))
+    LineProtocolCache.put(tracker.finalize('prediction_input'))
+
+  @classmethod
+  def _record_coco_categories(cls, predictions: List[Prediction]) -> None:
+    tracker: EventMetricsTracker[_CocoCategories] = EventMetricsTracker()
+    for prediction in predictions:
+      tracker.increment(_CocoCategories(prediction.label))
+    LineProtocolCache.put(
+        tracker.finalize('prediction_output', {
+            'model_image_size': _IMAGE_SIZE.value,
+            'model_precision': 'fp16' if _HALF_PRECISION.value else 'fp32',
+        }))
--- tests/test_yolopredictor.py
+++ tests/test_yolopredictor.py
@@ -1,11 +1,17 @@
-from unittest.mock import Mock
+import time
+from unittest.mock import Mock, patch
 
 from absl.testing import flagsaver, parameterized
+from line_protocol_cache.lineprotocolcache import LineProtocolCache
 
 from simple_jetson_nano_detection_server.prediction import Prediction
 from simple_jetson_nano_detection_server.yolopredictor import _HALF_PRECISION, _IMAGE_SIZE, YoloPredictor
 
+LINE_PROTOCOL_CACHE_PUT = Mock(return_value=None)
 
+
+@patch.object(LineProtocolCache, LineProtocolCache.put.__name__, LINE_PROTOCOL_CACHE_PUT)
+@patch.object(time, time.time_ns.__name__, Mock(return_value=1700000000000000000))
 class TestYoloPredictor(parameterized.TestCase):
 
   def setUp(self):
@@ -19,15 +25,18 @@ class TestYoloPredictor(parameterized.TestCase):
         [132.5, 104.5, 177.5, 141.875],
         [264.25, 173.40625, 319.75, 179.09375],
         [111.5, 164.5625, 319.5, 319.4375],
+        [111.5, 164.5625, 319.5, 319.4375],
     ]))
     mock_conf = Mock(tolist=Mock(return_value=[
         0.6460136771202087,
         0.42441198229789734,
         0.29746994376182556,
+        0.29746994376182556,
     ]))
     mock_cls = Mock(tolist=Mock(return_value=[
         1.0,
         2.0,
+        3.0,
         3.0,
     ]))
     mock_boxes = Mock(xyxy=mock_xyxy, conf=mock_conf, cls=mock_cls)
@@ -35,9 +44,11 @@ class TestYoloPredictor(parameterized.TestCase):
     mock_results = [mock_result]
 
     self.mock_yolo_predict = Mock(return_value=mock_results)
-    self.mock_yolo = Mock(predict=self.mock_yolo_predict, names={1: 'label-1', 2: 'label-2', 3: 'label-3'})
+    self.mock_yolo = Mock(predict=self.mock_yolo_predict, names={1: 'person', 2: 'bicycle', 3: 'car'})
 
     YoloPredictor.set_model(self.mock_yolo)
+
+    LINE_PROTOCOL_CACHE_PUT.reset_mock(return_value=True, side_effect=True)
 
     return super().setUp()
 
@@ -49,37 +60,58 @@ class TestYoloPredictor(parameterized.TestCase):
     YoloPredictor._model = None
 
     with self.assertRaisesWithLiteralMatch(Exception, 'A model must be set before prediciton'):
-      YoloPredictor.predict(b'')
+      YoloPredictor.predict(b'image-bytes')
+    LINE_PROTOCOL_CACHE_PUT.assert_not_called()
 
   def test_noResults_raises(self):
     self.mock_yolo_predict.return_value = []
 
     with self.assertRaisesWithLiteralMatch(Exception, 'There must be exactly 1 result, got 0 instead'):
-      YoloPredictor.predict(b'')
+      YoloPredictor.predict(b'image-bytes')
+    self.assertEqual(
+        [call_arg.args[0].to_line_protocol() for call_arg in LINE_PROTOCOL_CACHE_PUT.call_args_list],
+        ['prediction_input image_bytes=11i 1700000000000000000'],
+    )
 
   def test_moreThan1Results_raises(self):
     self.mock_yolo_predict.return_value = [Mock(), Mock()]
 
     with self.assertRaisesWithLiteralMatch(Exception, 'There must be exactly 1 result, got 2 instead'):
-      YoloPredictor.predict(b'')
+      YoloPredictor.predict(b'image-bytes')
+    self.assertEqual(
+        [call_arg.args[0].to_line_protocol() for call_arg in LINE_PROTOCOL_CACHE_PUT.call_args_list],
+        ['prediction_input image_bytes=11i 1700000000000000000'],
+    )
 
   def test_boxesIsNone_raises(self):
     self.mock_yolo_predict.return_value = [Mock(boxes=None)]
 
     with self.assertRaisesWithLiteralMatch(Exception, 'Boxes cannot be None'):
-      YoloPredictor.predict(b'')
+      YoloPredictor.predict(b'image-bytes')
+    self.assertEqual(
+        [call_arg.args[0].to_line_protocol() for call_arg in LINE_PROTOCOL_CACHE_PUT.call_args_list],
+        ['prediction_input image_bytes=11i 1700000000000000000'],
+    )
 
   def test_convertsToPredictions(self):
-    predictions = YoloPredictor.predict(b'')
+    predictions = YoloPredictor.predict(b'image-bytes')
 
     self.assertEqual(predictions, [
-        Prediction(x_min=132, x_max=177, y_min=104, y_max=141, label='label-1', confidence=0.6460136771202087),
-        Prediction(x_min=264, x_max=319, y_min=173, y_max=179, label='label-2', confidence=0.42441198229789734),
-        Prediction(x_min=111, x_max=319, y_min=164, y_max=319, label='label-3', confidence=0.29746994376182556),
+        Prediction(x_min=132, x_max=177, y_min=104, y_max=141, label='person', confidence=0.6460136771202087),
+        Prediction(x_min=264, x_max=319, y_min=173, y_max=179, label='bicycle', confidence=0.42441198229789734),
+        Prediction(x_min=111, x_max=319, y_min=164, y_max=319, label='car', confidence=0.29746994376182556),
+        Prediction(x_min=111, x_max=319, y_min=164, y_max=319, label='car', confidence=0.29746994376182556),
     ])
+    self.assertEqual(
+        [call_arg.args[0].to_line_protocol() for call_arg in LINE_PROTOCOL_CACHE_PUT.call_args_list],
+        [
+            'prediction_input image_bytes=11i 1700000000000000000',
+            'prediction_output,model_image_size=12345,model_precision=fp32 bicycle=1i,car=2i,person=1i 1700000000000000000'
+        ],
+    )
 
   def test_callsModelWithFlagValues(self):
-    YoloPredictor.predict(b'')
+    YoloPredictor.predict(b'image-bytes')
 
     call_args = self.mock_yolo_predict.call_args
     self.assertContainsSubset({'imgsz': 12345, 'half': False}, call_args.kwargs)
